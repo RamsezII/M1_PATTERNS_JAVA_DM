55cdffb66e7d1f58f976feeb9be4cf952f0c09fa
martin jules <smartties@hotmail.fr>
2023-03-27 11:53:52 +0200

    add java doc for memento + fix bug in memento + remove unecessary this in memento

--- src/command/Memento.java
+++ src/command/Memento.java
@@ -6,41 +6,64 @@ import java.util.ArrayList;
 
 /**
  * This class represents the Memento pattern.
+ * It allows us to store each state of the game
  */
 public class Memento {
+
+    //undoContainers is a list holding the X previous State of the game, it used when we want to undo changes and go back to previous states
     private ArrayList<ContainerForms> undoContainers;
+    //redoContainers is a list holding the X State that have undid, it used when we want to reapply the change we undid
     private ArrayList<ContainerForms> redoContainers;
 
+    /**
+     * This Constructor initialise two lists : an Undo list and a redo list
+     */
     public Memento(){
         this.undoContainers = new ArrayList<>();
         this.redoContainers = new ArrayList<>();
     }
-    
-    public void backup(ContainerForms containerForms){
-        this.undoContainers.add(containerForms.clone());
+
+    /**
+     * This backup function makes a deep copy of the State given in argument (which in our case, is always the current State before modifying it)
+     * The deepCopy with clone() is important, as in Java, objects are passed by reference, and in this base we don't want to hold reference in our list (that can be modified elsewhere in the code), but copies.
+     * Also if a backup is made and the redo list is not empty, we clear it because when new changes are made that not undo(s), we have a to clear it. Why ? Because redo list is just to undo the undo(s).
+     * @param currentState Is the current state of the game
+     */
+    public void backup(ContainerForms currentState){
+        this.undoContainers.add(currentState.clone());
         
         if(this.redoContainers.size() > 0)
             this.redoContainers.clear();
     }
 
-    public ContainerForms undo(ContainerForms currentContainerForm){
+    /**
+     * This function undo change : it store the current State into the redo list, pop the last State from the undo list, and return the new State which has been poped from the undo list
+     * @param currentState the current State of the game, which we put into the redo list
+     * @return the new State we want to apply to our Model
+     */
+    public ContainerForms undo(ContainerForms currentState){
         if(this.undoContainers.size() > 0){
             ContainerForms lastInserted = this.undoContainers.get(this.undoContainers.size()-1);
 
             this.undoContainers.remove(undoContainers.size()-1);
-            this.redoContainers.add(0, currentContainerForm);
+            this.redoContainers.add(0, currentState);
 
             return lastInserted;
         }
         return null;
     }
 
-    public ContainerForms redo(ContainerForms currentContainerForm){
+    /**
+     * This function redo change made after an undo : it store the current State into the undo list, pop the first State from the redo list, and return the new State which has been poped from the redo list
+     * @param currentState the current State of the game, which we put into the undo list
+     * @return the new State we want to apply to our Model
+     */
+    public ContainerForms redo(ContainerForms currentState){
         if(this.redoContainers.size() > 0){
             ContainerForms lastInserted = redoContainers.get(0);
             
             redoContainers.remove(0);
-            undoContainers.add(currentContainerForm);
+            undoContainers.add(currentState);
             
             return lastInserted;
         }
--- src/model/Model.java
+++ src/model/Model.java
@@ -17,15 +17,15 @@ public class Model {
      * The constructor of this class.
      */
     public Model(){
-        this.listener = null;
-        this.memento = new Memento();
-        this.containers = new ContainerForms();
+        listener = null;
+        memento = new Memento();
+        containers = new ContainerForms();
     }
 
     // Getter
     
     public ContainerForms getContainerForms(){
-        return this.containers;
+        return containers;
     }
     
     // Setters
@@ -36,51 +36,76 @@ public class Model {
 
    // Methods 
 
+    /**
+     * A function that add a circle to our containers, after it has been backup(containers) and notifity the view
+     * @param x center of our circle x
+     * @param y center of our cicle y
+     * @param radius radius of our circle
+     */
     public void createCircle(int x, int y, int radius){
-        this.containers.getListForms().add(new Circle(x, y, radius));
-        this.memento.backup(this.containers);
+        memento.backup(this.containers);
+        containers.getListForms().add(new Circle(x, y, radius));
         notifyView();
     }
 
+    /**
+     * A function that add a rectangle to our containers, after it has been backup(containers) and notifity the view
+     * @param x origin x
+     * @param y origin y
+     * @param w witdh
+     * @param h height
+     */
     public void createRectangle(int x, int y, int w, int h){
-        this.containers.getListForms().add(new Rectangle(x, y, w, h));
-        this.memento.backup(this.containers);
+        memento.backup(this.containers);
+        containers.getListForms().add(new Rectangle(x, y, w, h));
         notifyView();
     }
 
+    /**
+     * Delete all form which are not alive anymore and notify the view of a change
+     */
     public void deleteForm(){
         ArrayList<Form> list = new ArrayList<>();
-        for(Form fm : this.containers.getListForms()){
+        for(Form fm : containers.getListForms()){
             if(fm.isAlive() == false)
                 list.add(fm);
         }
         if(list.size() > 0){
-            this.memento.backup(this.containers);
-            this.containers.getListForms().removeAll(list);
+            memento.backup(containers);
+            containers.getListForms().removeAll(list);
         }
 
         notifyView();
     }
 
+    /**
+     * Undo change, and notifiy the View
+     */
     public void undo(){
         ContainerForms newContainer = this.memento.undo(this.containers);
         if(newContainer != null){
-            this.containers = newContainer;
+            containers = newContainer;
             notifyView();
         }
     }
-
+    /**
+     * Redo change that has been undid, and notifiy the View
+     */
     public void redo(){
-        ContainerForms newContainer = memento.redo(this.containers);
+        ContainerForms newContainer = memento.redo(containers);
         if(newContainer != null){
-            this.containers = newContainer;
+            containers = newContainer;
             notifyView();
         }
     }
 
+
+    /**
+     * Notify the view, so it can repaint all the forms
+     */
     public void notifyView(){
         if(listener != null)
-            this.listener.updatedModel(this);
+            listener.updatedModel(this);
     }
 }
 
